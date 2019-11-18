a37d0ceb15f965a6ea0e99c3ec94832176b7a9e9
Parina K <Parina.khennavong@gmail.com>
2019-11-18 15:39:53 +0100

    config done

--- routes/auth.js
+++ routes/auth.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const express = require("express");
+const router = new express.Router();
+const userModel = require("../models/user");
+const bcrypt = require("bcrypt");
+
+module.exports = router;
