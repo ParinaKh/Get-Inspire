d84914ace1c2570039b31300753df302a44206d4
helenita230 <helfav@outlook.fr>
2019-11-18 22:16:57 +0100

    layout, about & home css + navbar

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = new express.Router();
 
-router.get("/home", (req, res) => {
-    res.render("index")
+router.get("/home", (req, res, next) => {
+    res.render("index", { css: ["home", "layout"] });
 });
 
 
-router.get("/about", (req, res) => {
-    res.render("about")
+router.get("/about", (req, res, next) => {
+    console.log('in index.js');
+    res.render("about", { css: ["about", "layout"] });
 });
 
 
--- routes/inspire-me.js
+++ routes/inspire-me.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 const tripModel = require("./../models/trip");
 
 router.get("/inspire-me", (req, res) => {
-    res.render("inspire-me");
+    res.render("inspire-me", { css: ["inspire-me", "layout"] });
 });
 
 
