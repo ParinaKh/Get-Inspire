b7b3729b5c49e3086373380742ac96961ac566a7
Parina K <Parina.khennavong@gmail.com>
2019-11-20 18:19:59 +0100

    points added to map view

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,23 +1,37 @@
 var map;
+var bounds = new google.maps.LatLngBounds();
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    zoom: 10
   });
 }
 
 initMap();
 
-var request = {
-  query: "Muang Khong",
-  fields: ["name", "geometry"]
-};
-
 var service = new google.maps.places.PlacesService(map);
 
-service.findPlaceFromQuery(request, function(results, status) {
-  console.log(results.geometry);
-  //creer marqueur
+const oneStep = document.querySelectorAll(".step");
+oneStep.forEach(element => {
+  const cityName = element.textContent.trim();
+  console.log(cityName);
+  service.findPlaceFromQuery(
+    {
+      query: cityName,
+      fields: ["name", "geometry"]
+    },
+    function(results, status) {
+      console.log("premier");
+      // console.log("ici", results[0].geometry.location.lng());
+      const lat = results[0].geometry.location.lat();
+      const lng = results[0].geometry.location.lng();
+      var marker = new google.maps.Marker({
+        position: { lat: lat, lng: lng },
+        map: map,
+        title: cityName
+      });
+      bounds.extend(marker.position);
+      map.fitBounds(bounds);
+    }
+  );
 });
-
-//     map.setCenter(results[0].geometry.location);
