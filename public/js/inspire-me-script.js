5dc464d53a968e626e8784b54e9d68be85b857de
helene fav <helfav@outlook.fr>
2019-11-21 11:48:56 +0100

    favourites added + change to url discover

--- public/js/inspire-me-script.js
+++ public/js/inspire-me-script.js
@@ -1,33 +1,36 @@
 const hearts = document.querySelectorAll(".fas.fa-heart");
-var favorites = []
+var favourites = [];
+
+//afficher les voyages sauvegardés (coeur rouge)
+axios.get("/get-my-favourites").then(res => {
+    //avant d'arriver au then je passe dans le back
+    hearts.forEach(heart => {
+        let id = heart.getAttribute("data-id")
+        if (res.data.includes(id)) { heart.classList.toggle("clicked-heart"), favourites.push(id) }
+    })
+}).catch(err => console.log(err))
+
 
 //ajouter ou supprimer des favoris
-console.log(hearts);
 hearts.forEach(heart => {
     heart.onclick = function (event) {
         heart.classList.toggle("clicked-heart")
         const tripId = event.target.getAttribute("data-id");
-        if (heart.classList.contains("clicked-heart")) {
-            axios.post("/add-favourite", { tripId: tripId }).then(myAPIRes => {
+        if (heart.classList.contains("clicked-heart")) { //ajout favoris
+            favourites.push(tripId)
+            axios.post("/add-favourite", { favourites: favourites }).then(myAPIRes => {
                 const favouriteHearts = myAPIRes.data;
             }).catch(err => console.log(err))
         }
         else {
-            axios.post("/add-favourite", { tripId: tripId }).then(myAPIRes => {
+            favourites = favourites.filter(id => id !== tripId) // l'id c'est chaque élément des favoris
+            axios.post("/add-favourite", { favourites: favourites }).then(myAPIRes => {
                 const favouriteHearts = myAPIRes.data;
             }).catch(err => console.log(err))
         }
     }
 })
 
-//mémoriser les voyages sauvegardés pour afficher un coeur rouge
-axios.get("/my-favourites").then(res => {
-    console.log("hello", favorites)
-    hearts.forEach(heart => {
-        let id = heart.getAttribute("data-id")
-        if (res.data.includes(id)) { heart.classList.toggle("clicked-heart"), favorites.push(id) }
-    })
-}).catch(err => console.log(err))
 
 
 
--- routes/favourites.js
+++ routes/favourites.js
@@ -3,6 +3,8 @@ const router = new express.Router();
 const tripModel = require("../models/Trip");
 const userModel = require("../models/User");
 
+
+//display la page des favoris
 router.get("/my-favourites", (req, res) => {
 
     userModel.findById(req.session.currentUser._id).populate({
@@ -20,27 +22,19 @@ router.get("/my-favourites", (req, res) => {
 });
 
 
+router.get("/get-my-favourites", (req, res) => {
+    userModel.findById(req.session.currentUser._id).then(response => res.send(response.favourite)).catch(err => console.log(err))
+})
+
+
 router.post("/add-favourite", (req, res) => {
-    const tripId = req.body.tripId;
-    userModel.findOneAndUpdate({ _id: req.session.currentUser._id }, { $addToSet: { favourite: tripId } }) // addToSet = avoid push multiple times of the same trip 
+    const favourites = req.body.favourites;
+    userModel.findOneAndUpdate({ _id: req.session.currentUser._id }, { favourite: favourites }) // update ses favoris // addToSet = avoid push multiple times of the same trip 
         .then(dbRes => {
             res.send({ user: dbRes });
         })
         .catch(dbErr => console.error(dbErr));
 });
-
-router.get("/my-favourites", (req, res) => {
-    userModel.findById(req.session.currentUser._id).then(response => res.send(response.favourite)).catch(err => console.log(err))
-})
-
-// router.post("/delete-favourite", (req, res) => {
-//     const tripId = req.body.tripId;
-//     userModel.findOneAndRemove({ favourite: tripId }) // addToSet = avoid push multiple times of the same trip 
-//         .then(dbRes => {
-//             res.send({ user: dbRes });
-//         })
-//         .catch(dbErr => console.error(dbErr));
-// });
 
 
 
