dac827a98ea2d58042921fdd55570b54b680e7a0
Parina K <Parina.khennavong@gmail.com>
2019-11-21 15:23:37 +0100

    itinerary update

--- routes/Itinerary-temporary.js
+++ routes/Itinerary-temporary.js
@@ -4,21 +4,6 @@ const tripModel = require("../models/Trip");
 const uploader = require("./../config/cloudinary");
 const userModel = require("../models/User");
 
-// // PUBLIC ROUTES
-// router.get("/itinerary/:id", (req, res) => {
-//   tripModel
-//     .findOne({ _id: { $eq: req.params.id } })
-//     .populate("user")
-//     .then(dbRes => {
-//       res.render("itinerary", {
-//         trip: dbRes, // will be use in our trip.hbs view
-//         css: ["trip", "layout"],
-//         script: ["maps"]
-//       });
-//     })
-//     .catch(dbErr => console.log("err", dbErr));
-// });
-
 router.get("/trip/:id", (req, res) => {
   tripModel
     .findOne({ _id: { $eq: req.params.id } })
