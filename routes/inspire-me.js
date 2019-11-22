db1e228e49acdc664f927c646f668221611d49d6
camille <camille.grison@gmail.com>
2019-11-22 16:22:55 +0100

    route home redirection

--- routes/inspire-me.js
+++ routes/inspire-me.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = new express.Router();
 const tripModel = require("./../models/Trip");
 
-router.get("/inspire-me", (req, res) => {
+router.get(["/", "/inspire-me"], (req, res) => {
     tripModel.find()
         .populate({
             path: "user",
